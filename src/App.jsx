1f77fb2413e1f3cde09bd4037a3ead9928609731
annawaterhouse <annawaterhouse05@gmail.com>
2023-10-10 15:58:12 -0400

    save conflict

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,18 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import '{puppyList} from './data.js'
-import '{ useState } from 'react'
-
-
+import { puppyList } from './data.js';
+import { useState } from 'react';
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
-  console.log(puppyList);
   return (
     <div>
-{
-  puppies.map((puppy) => {
-    return <p>{puppy.name}</p>
-  })
-}
+      {
+        puppies.map((puppy) => {
+          return <p>{puppy.name}</p>
+        })
+      }
     </div>
   )
 }
