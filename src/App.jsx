02d2cc00bc254456d1b6f559f7861fe9ea8e874d
zhiarTX <138315070+zhiarTX@users.noreply.github.com>
2023-10-10 14:32:23 -0600

    change
    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+// import viteLogo from '/vite.svg'gi
 import './App.css'
 import { puppyList } from './data.js';
 import { useState } from 'react';
