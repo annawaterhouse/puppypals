6cafcee455f480ff1d6bd69545219f9df9e64cea
annawaterhouse <annawaterhouse05@gmail.com>
2023-10-10 16:28:20 -0400

    add more functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import React from 'react';
 import { puppyList } from './data.js';
 import { useState } from 'react';
 
 function App() {
-  const [puppies, setPuppies] = useState(puppyList)
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  console.log('puppies', puppies);
   return (
     <div>
-      {
-        puppies.map((puppy) => {
-          return <p>{puppy.name}</p>
-        })
-      }
+      <p>{featPupId}</p>;
+      {puppies.map((puppy) => {
+          return (
+            <p onClick = { () => { setFeatPupId(puppy.id) }} key = {puppy.id}>
+            {puppy.name}
+          </p>)
+        })}
     </div>
   )
 }
